8236c487b2bd2094ef49222c19f6d0ff21002b13
Sammy1301 <sammy1301m@gmail.com>
2026-05-01 08:44:33 -0400

    fix captcha

--- src/providers/CaptchaProvider.tsx
+++ src/providers/CaptchaProvider.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
 export function CaptchaProvider({ children }: { children: React.ReactNode }) {
-  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
+  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '6Le6n8QsAAAAAAL97q1H2G7icw99qI-DKWXmx2vV'
 
   if (!siteKey) {
     console.warn('reCAPTCHA site key is missing. Captcha will not be enabled.')
